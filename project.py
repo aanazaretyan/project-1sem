5ed87a367642a6b2ae20d30a7d2e828db726b271
alfaxa4 <aanazaretyan@miem.hse.ru>
2020-12-20 16:19:11 +0300

    добавил табуляцию в игре Миши

--- project.py
+++ project.py
@@ -343,175 +343,175 @@ elif a == 2:
 
 elif a == 3:
     pygame.init()
-# создаем игровое окно
-win = pygame.display.set_mode((1000,700))
-# заголовок
-pygame.display.set_caption("Игра на питоне")
+    # создаем игровое окно
+    win = pygame.display.set_mode((1000,700))
+    # заголовок
+    pygame.display.set_caption("Игра на питоне")
 
-font = pygame.font.Font(None,20)
-text = font.render('Hello! This is my first game! Here are some feachers:', True, [255,255,255])
-textscopes = (40,10)
-text2 = font.render('You are able to move with these bottoms ( <-  ^  -> )', True, [255,255,255])
-text3 = font.render('Also it is possible to run with (SHIFT) and shoot with bottom (F)', True, [255,255,255])
+    font = pygame.font.Font(None,20)
+    text = font.render('Hello! This is my first game! Here are some feachers:', True, [255,255,255])
+    textscopes = (40,10)
+    text2 = font.render('You are able to move with these bottoms ( <-  ^  -> )', True, [255,255,255])
+    text3 = font.render('Also it is possible to run with (SHIFT) and shoot with bottom (F)', True, [255,255,255])
 
-# добавление спрайтов
-walk_right = [pygame.image.load('Sprites/pygame_right_1.png'),
-pygame.image.load('Sprites/pygame_right_2.png'),pygame.image.load('Sprites/pygame_right_3.png'),
-pygame.image.load('Sprites/pygame_right_4.png'),pygame.image.load('Sprites/pygame_right_5.png'),
-pygame.image.load('Sprites/pygame_right_6.png')]
+    # добавление спрайтов
+    walk_right = [pygame.image.load('Sprites/pygame_right_1.png'),
+    pygame.image.load('Sprites/pygame_right_2.png'),pygame.image.load('Sprites/pygame_right_3.png'),
+    pygame.image.load('Sprites/pygame_right_4.png'),pygame.image.load('Sprites/pygame_right_5.png'),
+    pygame.image.load('Sprites/pygame_right_6.png')]
 
-walk_left = [pygame.image.load('Sprites/pygame_left_1.png'),
-pygame.image.load('Sprites/pygame_left_2.png'),pygame.image.load('Sprites/pygame_left_3.png'),
-pygame.image.load('Sprites/pygame_left_4.png'),pygame.image.load('Sprites/pygame_left_5.png'),
-pygame.image.load('Sprites/pygame_left_6.png')]
+    walk_left = [pygame.image.load('Sprites/pygame_left_1.png'),
+    pygame.image.load('Sprites/pygame_left_2.png'),pygame.image.load('Sprites/pygame_left_3.png'),
+    pygame.image.load('Sprites/pygame_left_4.png'),pygame.image.load('Sprites/pygame_left_5.png'),
+    pygame.image.load('Sprites/pygame_left_6.png')]
 
-play_stand = pygame.image.load('Sprites/pygame_idle.png')
+    play_stand = pygame.image.load('Sprites/pygame_idle.png')
 
-def texting():
-    ''' Функция для проявления надписей'''
-    win.blit(text,textscopes)
-    win.blit(text2,(40,30))
-    win.blit(text3,(40,50))
+    def texting():
+        ''' Функция для проявления надписей'''
+        win.blit(text,textscopes)
+        win.blit(text2,(40,30))
+        win.blit(text3,(40,50))
 
-class snaryad():
-    '''Класс, описывающий снаряд'''
-    def __init__(self,x,y,r,colour,direction):
-        self.x = x
-        self.y = y
-        self.r = r
-        self.colour = colour
-        self.direction = direction
-        self.bulletSpeed = 8 * direction
+    class snaryad():
+        '''Класс, описывающий снаряд'''
+        def __init__(self,x,y,r,colour,direction):
+            self.x = x
+            self.y = y
+            self.r = r
+            self.colour = colour
+            self.direction = direction
+            self.bulletSpeed = 8 * direction
 
-    def draw(self, win):
-        pygame.draw.circle(win,self.colour,(self.x,self.y),self.r)
+        def draw(self, win):
+            pygame.draw.circle(win,self.colour,(self.x,self.y),self.r)
 
-clock = pygame.time.Clock()
+    clock = pygame.time.Clock()
 
-def lowSpeed():
-    """
-    def lowspeed() позволяет персонажу снизить скорость,
-        если он движется бустрее его изначальной скорости
-    """
-    global speed
-    if speed > 5:
-        speed -= 5
+    def lowSpeed():
+        """
+        def lowspeed() позволяет персонажу снизить скорость,
+            если он движется бустрее его изначальной скорости
+        """
+        global speed
+        if speed > 5:
+            speed -= 5
 
-def drawWindow():
-    """
-    def drawWindow() заполняет экран черным цветом и меняет спрайт в зависимости от переменной aimCount    
-    """
-    global aimCount
-     # после каждого перемещения нужно заполнять поле черным цветом
-    win.fill((0,0,0))
-    # создание объекта
-    if aimCount + 1 >= 30:
-        aimCount = 0
-    if left:
-        win.blit(walk_left[aimCount // 5], (x, y))
-        aimCount += 1
-    elif right:
-        win.blit(walk_right[aimCount // 5], (x, y))
-        aimCount += 1
-    else:
-        win.blit(play_stand, (x, y))
-    for bullet in bullets:
-        bullet.draw(win)
-    texting()
-    
-    pygame.display.update()
-
-#параметры
-x = 50
-y : float = 700 - 76
-width = 60
-height = 71
-speed = 5
-speedCount = 10
-
-isJump = False
-jumpCount = 10
-aimCount = 0
-
-right = False
-left = False
-
-bullets : list = []
-lastMove = 'right'
-direction = 1
-#coolDown = 0
-
-WHITE = (255, 255, 255)
-RED = (225, 0, 50)
-GREEN = (0, 225, 0)
-BLUE = (0, 0, 225)
-
-# цикл самой игры
-run = True
-while run:
-    # время через которое цикл будет обновляться
-    clock.tick(30)
-
-    for bullet in bullets:
-        if bullet.x < 1000 and bullet.x > 0:
-            bullet.x += bullet.bulletSpeed
+    def drawWindow():
+        """
+        def drawWindow() заполняет экран черным цветом и меняет спрайт в зависимости от переменной aimCount    
+        """
+        global aimCount
+        # после каждого перемещения нужно заполнять поле черным цветом
+        win.fill((0,0,0))
+        # создание объекта
+        if aimCount + 1 >= 30:
+            aimCount = 0
+        if left:
+            win.blit(walk_left[aimCount // 5], (x, y))
+            aimCount += 1
+        elif right:
+            win.blit(walk_right[aimCount // 5], (x, y))
+            aimCount += 1
         else:
-            bullets.pop(bullets.index(bullet))
+            win.blit(play_stand, (x, y))
+        for bullet in bullets:
+            bullet.draw(win)
+        texting()
+        
+        pygame.display.update()
 
-    # опишем структуру выхода из программы
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+    #параметры
+    x = 50
+    y : float = 700 - 76
+    width = 60
+    height = 71
+    speed = 5
+    speedCount = 10
 
-    # отслеживание нажатий 
-    keys = pygame.key.get_pressed()
+    isJump = False
+    jumpCount = 10
+    aimCount = 0
 
-    # определение последнего направления
-    if lastMove == 'right':
-        direction = 1
-    else:
-        direction = -1
+    right = False
+    left = False
 
-    # запуск снаряда
-    if keys[pygame.K_f]:
-        if len(bullets) < 10:
-                bullets.append(snaryad(round(x + width // 2), round(y + height // 2), 
-                5, RED, direction))
+    bullets : list = []
+    lastMove = 'right'
+    direction = 1
+    #coolDown = 0
 
-            
-    # управление персонажем
-    if keys[pygame.K_LSHIFT]:
-        speed += 5
-    if keys[pygame.K_LEFT] and x > 5:
-        x -= speed
-        left = True
-        right = False
-        lastMove = 'left'
-    elif keys[pygame.K_RIGHT] and x < 1000 - width - 5:
-        x += speed
-        left = False
-        right = True
-        lastMove = 'right'
-    else:
-        left = False
-        right = False
-        aimCount = 0
-    if not(isJump):
-        if keys[pygame.K_UP]:
-            isJump = True
-    else:
-        if jumpCount >= -10:
-            if jumpCount < 0:
-                y += (jumpCount ** 2) / 2
+    WHITE = (255, 255, 255)
+    RED = (225, 0, 50)
+    GREEN = (0, 225, 0)
+    BLUE = (0, 0, 225)
+
+    # цикл самой игры
+    run = True
+    while run:
+        # время через которое цикл будет обновляться
+        clock.tick(30)
+
+        for bullet in bullets:
+            if bullet.x < 1000 and bullet.x > 0:
+                bullet.x += bullet.bulletSpeed
             else:
-                y -= (jumpCount ** 2) / 2
-            jumpCount -= 1
-        else:
-            isJump = False
-            jumpCount = 10
-    lowSpeed()
-    drawWindow()
+                bullets.pop(bullets.index(bullet))
 
-    
-# на случай если программа не вышла прописываем функцию выхода еще раз
-pygame.quit()
+        # опишем структуру выхода из программы
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        # отслеживание нажатий 
+        keys = pygame.key.get_pressed()
+
+        # определение последнего направления
+        if lastMove == 'right':
+            direction = 1
+        else:
+            direction = -1
+
+        # запуск снаряда
+        if keys[pygame.K_f]:
+            if len(bullets) < 10:
+                    bullets.append(snaryad(round(x + width // 2), round(y + height // 2), 
+                    5, RED, direction))
+
+                
+        # управление персонажем
+        if keys[pygame.K_LSHIFT]:
+            speed += 5
+        if keys[pygame.K_LEFT] and x > 5:
+            x -= speed
+            left = True
+            right = False
+            lastMove = 'left'
+        elif keys[pygame.K_RIGHT] and x < 1000 - width - 5:
+            x += speed
+            left = False
+            right = True
+            lastMove = 'right'
+        else:
+            left = False
+            right = False
+            aimCount = 0
+        if not(isJump):
+            if keys[pygame.K_UP]:
+                isJump = True
+        else:
+            if jumpCount >= -10:
+                if jumpCount < 0:
+                    y += (jumpCount ** 2) / 2
+                else:
+                    y -= (jumpCount ** 2) / 2
+                jumpCount -= 1
+            else:
+                isJump = False
+                jumpCount = 10
+        lowSpeed()
+        drawWindow()
+
+        
+    # на случай если программа не вышла прописываем функцию выхода еще раз
+    pygame.quit()

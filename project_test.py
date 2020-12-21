40e5b3794f1849de4f8e584cc4853043567d3c8e
KarenYer <75987919+KarenYer@users.noreply.github.com>
2020-12-21 09:23:23 +0300

    'main'

--- project_test.py
+++ project_test.py
@@ -31,6 +31,26 @@ class snaryadTest(unittest.TestCase):
         bullet = snaryad(x,y,r,colour,direction)
         bullet.movement(1)
         self.assertAlmostEqual((bullet.x,bullet.y), (42, 700 - 76))
+    def test_positive_speed(self):
+        x = 50
+        y = 700 - 76
+        colour = (50,50,50)
+        r = 5
+        direction = 1
+        bullet = snaryad(x,y,r,colour,direction)
+        a=bullet.movement(2)
+        b=bullet.movement(1)
+        self.assertAlmostEqual(b-a, 8)
+    def test_negative_speed(self):
+        x = 50
+        y = 700 - 76
+        colour = (50,50,50)
+        r = 5
+        direction = -1
+        bullet = snaryad(x,y,r,colour,direction)        
+        a=bullet.movement(2)
+        b=bullet.movement(1)
+        self.assertAlmostEqual(b-a, -8)
 
 if __name__=='__main__':
     unittest.main()
